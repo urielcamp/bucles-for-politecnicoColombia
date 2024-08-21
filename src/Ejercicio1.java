95248c54bc66af710d6ef6b2d4f7a5deb95c9d38
Uriel Campaña <urielcamp@gmail.com>
2024-08-21 15:37:13 -0300

    ejercicio 1

---
+++ src/Ejercicio1.java
@@ -0,0 +1,17 @@
+public class Ejercicio1 {
+
+    public static void main(String[] args) {
+
+
+        int num = 5;
+        int sum = 1;
+
+        for (int i = 1; i <= num; i++){
+            sum = sum * i;
+
+        }
+
+        System.out.println(sum);
+    }
+
+}
