92e89a1cdfa3bfec349491b032d12bebda8a2da1
Uriel Campaña <urielcamp@gmail.com>
2024-08-21 15:43:34 -0300

    ejercicio 2

---
+++ src/Ejercicio2.java
@@ -0,0 +1,15 @@
+public class Ejercicio2 {
+
+    public static void main(String[] args) {
+
+        int num = 40;
+
+
+        for (int i = 1; i < num ; i++) {
+            if (num%i == 0){
+                System.out.println(i);
+            }
+        }
+
+    }
+}
